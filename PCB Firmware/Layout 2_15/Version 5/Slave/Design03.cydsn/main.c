2aa0c2cbd6b28afb238d35be5b8871f3cc4ba1e5
Steven Noyce <steven.noyce@gmail.com>
2019-10-17 15:44:53 -0400

    Copied over main.c for all versions of the selector chip

--- PCB Firmware/Layout 2_15/Version 5/Slave/Design03.cydsn/main.c
+++ PCB Firmware/Layout 2_15/Version 5/Slave/Design03.cydsn/main.c
@@ -1,26 +1,53 @@
-/* ========================================
- *
- * Copyright YOUR COMPANY, THE YEAR
- * All Rights Reserved
- * UNPUBLISHED, LICENSED SOFTWARE.
- *
- * CONFIDENTIAL AND PROPRIETARY INFORMATION
- * WHICH IS THE PROPERTY OF your company.
- *
- * ========================================
-*/
 #include "project.h"
 
-int main(void)
-{
-    CyGlobalIntEnable; /* Enable global interrupts. */
+#define SELECTOR_ID (3)
 
-    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+#define SELECTOR_COUNT (4u)
+#define INTERMEDIATE_COUNT (4u)
+#define CONTACT_COUNT (64u)
+#define CONTACT_CONNECT_CODE (0xC0u)
+#define CONTACT_DISCONNECT_CODE (0xD1u)
 
-    for(;;)
-    {
-        /* Place your application code here. */
-    }
+struct Selector_I2C_Struct {
+	struct {
+		uint8 subAddress;
+		uint8 data[126];
+	} write;
+	struct {
+		uint8 subAddress;
+		uint8 data[126];
+	} read;
+	uint8 busAddress;
+};
+
+int main(void) {
+	CyGlobalIntEnable;
+	
+	struct Selector_I2C_Struct selector;
+	uint8 I2C_Bus_Addresses[SELECTOR_COUNT] = {0x66, 0x11, 0x44, 0x22};
+
+	// Start all hardware blocks
+	//AMux_1_Start(); //not required for a passive block
+	Opamp_1_Start();
+	ADC_SAR_Seq_1_Start();
+	
+	EZI2C_1_Start();
+	EZI2C_1_EzI2CSetAddress1(I2C_Bus_Addresses[SELECTOR_ID]);
+	EZI2C_1_EzI2CSetBuffer1(sizeof(selector), sizeof(selector), (uint8*) &selector);
+	
+	while(1) {
+		for (uint8 i = 0; i < sizeof(selector.write.data); i++) {
+			if (i < AMux_1_CHANNELS) {
+				if (selector.write.data[i] == CONTACT_CONNECT_CODE) {
+					AMux_1_Connect(i);
+				}
+				
+				if (selector.write.data[i] == CONTACT_DISCONNECT_CODE) {
+					AMux_1_Disconnect(i);
+				}
+			}
+		}
+	}
 }
 
-/* [] END OF FILE */
+
